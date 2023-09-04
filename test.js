ae64ec24fb058445bada591ef1ca48d846f4ba48
mico delphin <delphinmicomyiza@gmail.com>
2023-09-04 17:10:45 +0200

    third commit

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-type', 'text/plain');
-res.end('del first troooo!\n');
+res.end('del first troooo fresh fresh!\n');
 });
 
 server.listen(port, hostname, () => {
